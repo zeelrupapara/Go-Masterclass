af67b59c485b4c3b9b7fdcee119784ee4db9182c
zeelrupapara <zeelrupapara@gmail.com>
2024-11-25 11:20:08 +0530

    fix: remove pingpong from main

--- main.go
+++ main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"fmt"
-
-	pingpong "github.com/zeelrupapara/masterclass/Ping-Pong"
 )
 
 // Basic Hello World Program
 func main() {
 	fmt.Println("Hello World")
-	pingpong.PingPong(2)
 }
